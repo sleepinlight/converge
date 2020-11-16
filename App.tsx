1365fa25dd1d9a917098480d9e0f5ef31bd6392f
Andy Carter <acarter264@gmail.com>
2020-11-16 14:57:42 -0600

    firebase email auth

--- App.tsx
+++ App.tsx
@@ -1,23 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+// import useCachedResources from './hooks/useCachedResources';
+// import useColorScheme from './hooks/useColorScheme';
+// import Navigation from './navigation';
+
+// export default function App() {
+//   const isLoadingComplete = useCachedResources();
+//   const colorScheme = useColorScheme();
+
+//   if (!isLoadingComplete) {
+//     return null;
+//   } else {
+//     return (
+//       <SafeAreaProvider>
+//         <Navigation colorScheme={colorScheme} />
+//         <StatusBar />
+//       </SafeAreaProvider>
+//     );
+//   }
+// }
+
+
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react'
+import { Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { LoginScreen, HomeScreen, RegistrationScreen } from './screens';
+import {decode, encode} from 'base-64'
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+import { firebase } from './firebase/config';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData: any = document.data();
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
+
+  if (loading) {	
+    return (	
+      <View>
+            <Text>Loading Screen</Text>
+        </View>
+    )	
   } else {
     return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          { user ? (
+            <Stack.Screen name="Home">
+              {props => <HomeScreen {...props} extraData={user} />}
+            </Stack.Screen>
+          ) : (
+            <>
+              <Stack.Screen name="Login" component={LoginScreen} />
+              <Stack.Screen name="Registration" component={RegistrationScreen} />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     );
   }
+
+  
+
+  
 }
---
+++ firebase/config.tsx
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBXfzUBio9NiucWsV6VTli1L4i8eRjSzW4",
+    authDomain: "mindreader-ec661.firebaseapp.com",
+    databaseURL: "https://mindreader-ec661.firebaseio.com",
+    projectId: "mindreader-ec661",
+    storageBucket: "mindreader-ec661.appspot.com",
+    messagingSenderId: "1077624981050",
+    appId: "1:1077624981050:web:06146ca7692e02fb8e9703",
+    measurementId: "G-WM018HBJ0V"
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
